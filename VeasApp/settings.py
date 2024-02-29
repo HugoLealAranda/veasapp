439ea3f7e4296e722391b7505bf584cae6280ff1
hugolealaranda <hugoleal@pucv.cl>
2024-02-28 23:26:44 -0300

    asd

--- VeasApp/settings.py
+++ VeasApp/settings.py
@@ -61,7 +61,7 @@ ROOT_URLCONF = 'VeasApp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [r'C:\Users\hugonote\Desktop\mi_entorno_virtual\VeasApp\gestionPedidos\templates'],
+        'DIRS': [os.path.join(BASE_DIR, 'gestionPedidos/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
