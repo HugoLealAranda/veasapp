7a9f0decd056f5b567846f9d8ac8f7495d875a11
hugolealaranda <hugoleal@pucv.cl>
2024-03-28 21:07:15 -0300

    arreglo logicas

--- gestionPedidos/views.py
+++ gestionPedidos/views.py
@@ -316,12 +316,12 @@ def buscar(request):
                 valores_unitarios_venta_rental_veas = [float(articulo.valor_unitario) for articulo in articulos_venta_rental_veas]
 
                 # Obtener los datos para cuando Rental Veas cotiza a sus clientes
-                articulos_cotizacion_rental_veas = articulos.exclude(numero_cotizacion__isnull=True).exclude(fecha__isnull=True)
+                articulos_cotizacion_rental_veas = articulos.filter(numero_cotizacion__isnull=False).filter(empresa_vendedora='rental veas').exclude(fecha__isnull=True)
                 fechas_cotizacion_rental_veas = [articulo.fecha.strftime('%Y-%m-%d') for articulo in articulos_cotizacion_rental_veas]
                 valores_unitarios_cotizacion_rental_veas = [float(articulo.valor_unitario) for articulo in articulos_cotizacion_rental_veas]
 
                 # Obtener los datos para cuando los proveedores cotizan a Rental Veas
-                articulos_cotizacion_proveedores = articulos.filter(empresa_compradora='rental veas').exclude(numero_cotizacion__isnull=True).exclude(fecha__isnull=True)
+                articulos_cotizacion_proveedores = articulos.filter(numero_cotizacion__isnull=False).filter(empresa_compradora='rental veas').exclude(fecha__isnull=True)
                 fechas_cotizacion_proveedores = [articulo.fecha.strftime('%Y-%m-%d') for articulo in articulos_cotizacion_proveedores]
                 valores_unitarios_cotizacion_proveedores = [float(articulo.valor_unitario) for articulo in articulos_cotizacion_proveedores]
 
@@ -329,14 +329,14 @@ def buscar(request):
                 #preparar los datos para el grafico de lineas
                 fechas_rental_veas = []
                 valores_unitarios_rental_veas = []
-                articulos_rental_veas = articulos.filter(empresa_vendedora='Rental Veas').exclude(fecha__isnull=True)
+                articulos_rental_veas = articulos.filter(empresa_vendedora='rental veas').exclude(fecha__isnull=True)
 
                 if articulos_rental_veas:
                     fechas_rental_veas = [articulo.fecha.strftime('%Y-%m-%d') for articulo in articulos_rental_veas]
                     valores_unitarios_rental_veas = [float(articulo.valor_unitario) for articulo in articulos_rental_veas] 
                 fechas_otros = []
                 valores_unitarios_otros = []
-                articulos_otros = articulos.exclude(empresa_vendedora='Rental Veas').exclude(fecha__isnull=True)
+                articulos_otros = articulos.exclude(empresa_vendedora='rental veas').exclude(fecha__isnull=True)
 
                 if articulos_otros:
                     fechas_otros = [articulo.fecha.strftime('%Y-%m-%d') for articulo in articulos_otros]
